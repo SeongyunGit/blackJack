c067bf7f2fd193ef2d3051c2cae1ef34fd6f5a66
조성윤 <okks3579@gmail.com>
2024-10-11 17:46:05 +0900

    랜덤 함수 구현

--- src/main/java/BlackJack/Application.java
+++ src/main/java/BlackJack/Application.java
@@ -1,10 +1,7 @@
 package BlackJack;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,6 +15,17 @@ public class Application {
             System.out.println(nameList.get(i) + "의 베팅 금액은?");
             moneyList.add(sc.nextInt());
         }
+        String[] CardName = {"클로버","스페이드","하트","다이아몬드"};
+        String[] number = {"A","1","2","3","4","5","6","7","8","9","J","Q","K"};
+
+        for(int i=0;i< nameList.size();i++) {
+            System.out.println(nameList.get(i) + "는 한장의 카드를 더 받겠습니까? (예는 y, 아니오는 n");
+            String answer=sc.nextLine();
+
+        }
+        System.out.println();
+        Random random = new Random();
+
 
 
     }
