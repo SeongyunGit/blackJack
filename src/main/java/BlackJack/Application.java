d9b9aa6ccb6687d78b8d991247af663edec3c026
조성윤 <okks3579@gmail.com>
2024-10-11 17:20:16 +0900

    이름입력 구현

--- src/main/java/BlackJack/Application.java
+++ src/main/java/BlackJack/Application.java
@@ -1,7 +1,16 @@
 package BlackJack;
 
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        List<String> nameList = Arrays.asList(input.split(","));
+        System.out.println(nameList);
     }
 }
